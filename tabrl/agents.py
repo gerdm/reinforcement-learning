c7b1cc7336da20cd4a8d04007cb87204b351e2a0
Gerardo Durán Martín <g.duran@me.com>
2025-07-19 08:48:56 +0100

    ch06-feat: gridworld examples refactor

--- tabrl/agents.py
+++ tabrl/agents.py
@@ -14,7 +14,7 @@ def get_probas(qval, epsilon):
     """
     qmax = qval.max()
     n_actions = len(qval)
-    mask = qmax == qval
+    mask = qmax == qval #  mask for equal values
 
     if np.all(mask):
         # Random selection
@@ -35,132 +35,78 @@ def get_probas(qval, epsilon):
 
 @njit
 def choose_action(ix, Q, epsilon):
+    """
+    Choose action based on epsilon-greedy
+    """
     probas = get_probas(Q[ix], epsilon)
     action = np.random.multinomial(1, probas).argmax()
     return action
 
 
 @njit
-def sarsa_step(s, a, r, s_next, a_next, Q, epsilon, alpha, gamma):
+def choose_action_double_q(ix, Qs, epsilon):
     """
-    SARSA step
+    Choose action based on average of Q1 and Q2
+    and epsilon-greedy.
+    Use only for double-Q learning
+    """
+    Q1, Q2 = Qs[0], Qs[1]
+    Q = (Q1 + Q2) / 2
+    action = choose_action(ix, Q, epsilon)
+    return action
+
+
+@njit
+def sarsa_update(s, a, r, s_next, a_next, Q, epsilon, alpha, gamma):
+    """
+    SARSA update
     :(s, a) -> (r, s_new, a_new):
     """
-    q_new = Q[s, a] + alpha * (r + gamma * Q[s_next, a_next] - Q[s, a])
-    return q_new
+    q_next = Q[s, a] + alpha * (r + gamma * Q[s_next, a_next] - Q[s, a])
+    Q[s, a] = q_next
+    return Q
 
 
 @njit
-def expected_sarsa_step(s, Q, epsilon, alpha, gamma, gridworld, movements):
+def expected_sarsa_update(s, a, r, s_next, a_next, Q, epsilon, alpha, gamma):
     """
-    :Expected SARSA step:
-    s -> a -> (s_new, r_new)
+    Expected SARSA update
+    :(s, a) -> (r, s_new, a_new):
     """
-    # choose action 'a' based on Q and 's'
-    a = choose_action(s, Q, epsilon)
-    step = movements[a]
-    # observe 'r' and 's_new'
-    s_new, r = gridworld.move_and_reward(s, step)
-
-    # Expected value of Q[s_new, :]
-    probas = get_probas(Q[s_new], epsilon)
-    q_new = Q[s, a] + alpha * (r + gamma * (probas * Q[s_new, :]).sum() - Q[s, a])
-    return (r, s_new, a), q_new
+    # Expected value of Q[s_next, :]
+    probas = get_probas(Q[s_next], epsilon)
+    q_next = Q[s, a] + alpha * (r + gamma * (probas * Q[s_next, :]).sum() - Q[s, a])
+    Q[s, a] = q_next
+    return Q
 
 
 @njit
-def qlearning_step(s, Q, epsilon, alpha, gamma, gridworld, movements):
+def qlearning_update(s, a, r, s_next, a_next, Q, epsilon, alpha, gamma):
     """
-    :Q-learning step:
-    s -> a -> (s_new, r_new)
+    s -> a -> (s_new, r)
     """
-    # choose action 'a' based on Q and 's'
-    a = choose_action(s, Q, epsilon)
-    step = movements[a]
-    # observe 'r' and 's_new'
-    s_new, r = gridworld.move_and_reward(s, step)
-
-    q_new = Q[s, a] + alpha * (r + gamma * Q[s_new, :].max() - Q[s, a])
-    return (r, s_new, a), q_new
+    q_next = Q[s, a] + alpha * (r + gamma * Q[s_next, :].max() - Q[s, a])
+    Q[s, a] = q_next
+    return Q
 
 
 @njit
-def double_q_learning_step(s, Qs, epsilon, alpha, gamma, gridworld, movements):
+def double_q_learning_update(s, a, r, s_next, a_next, Qs, epsilon, alpha, gamma):
     """
     We have a duplicate Q-value function Qs = [Q1, Q2].
-    At each step, we flip a coin to decide which Q-value function to update and provide an estimate
+    At each update, we flip a coin to decide which Q-value function to update and provide an estimate
     of the action.  The other Q-value function is used to choose the action.
     """
     iq = np.random.randint(2)
     Q1, Q2 = Qs[iq], Qs[1 - iq]
 
-    # choose action 'a' based on average of Q1 and Q2 at state 's'
-    a = choose_action(s, (Q1 + Q2) / 2, epsilon)
-    step = movements[a]
-
-    # observe 'r' and 's_new'
-    s_new, r = gridworld.move_and_reward(s, step)
-
     # Update Q1 using Q2 to determine the value of the action
-    q_new = Q1[s, a] + alpha * (r + gamma * Q2[s_new, Q1[s_new, :].argmax()] - Q1[s, a])
-    return (r, s_new, a), q_new, iq
+    q_next = Q1[s, a] + alpha * (r + gamma * Q2[s_next, Q1[s_next, :].argmax()] - Q1[s, a])
+    Qs[iq, s, a] = q_next
+    return Qs
 
 
 @njit
-def sarsa_step_and_update(
-    s, a, Q, epsilon, alpha, gamma, gridworld, movements
-):
-    Q = Q.copy()
-
-    # take action 'a', observe 'r' and s_new'
-    step = movements[a]
-    s_new, r = gridworld.move_and_reward(s, step)
-    # Choose 'a_new' based on Q and 's_new'
-    a_new = choose_action(s_new, Q, epsilon)
-    
-    (r, s_new, a_new), q_new = sarsa_step(s, a, Q, epsilon, alpha, gamma, gridworld, movements)
-    Q[s, a] = q_new
-    return (r, s_new, a_new), Q
-
-
-@njit
-def expected_sarsa_step_and_update(
-    s, a, Q, epsilon, alpha, gamma, gridworld, movements
-):
-    Q = Q.copy()
-    (r, s_new, a), q_new = expected_sarsa_step(s, Q, epsilon, alpha, gamma, gridworld, movements)
-    Q[s, a] = q_new
-    return (r, s_new, a), Q
-
-
-@njit
-def qlearning_step_and_update(
-    s, a_prev, Q, epsilon, alpha, gamma, gridworld, movements
-):
-    """
-    Note: a_prev is not used in the function,
-    but it is included for compatibility with the SARSA function
-    """
-    Q = Q.copy()
-    (r, s_new, a), q_new = qlearning_step(s, Q, epsilon, alpha, gamma, gridworld, movements)
-    Q[s, a] = q_new
-    return (r, s_new, a), Q
-
-
-@njit
-def double_q_learning_step_and_update(
-    s, a_prev, Qs, epsilon, alpha, gamma, gridworld, movements
-):
-    """
-    Note: a_prev is not used in the function,
-    but it is included for compatibility with the SARSA function
-    """
-    Qs = Qs.copy()
-    (r, s_new, a), q_new, iq = double_q_learning_step(s, Qs, epsilon, alpha, gamma, gridworld, movements)
-    Qs[iq, s, a] = q_new
-    return (r, s_new, a), Qs
-
-
 def step_agent(s, a, gridworld, movements):
     """
     Take action 'a', observe reward 'r' and new state 's_new'
@@ -170,13 +116,42 @@ def step_agent(s, a, gridworld, movements):
     return r, s_new
 
 
+@njit
+def step_and_update_agent(Q, s, a, epsilon, alpha, gamma, gridworld, movements, update_fn, policy_fn):
+    """
+    Parameters
+    ---------
+    epsilon: (0, 1)
+        exploration rate
+    alpha: (0,1), float
+        learning rate
+    gamma: (0,1), float
+        discount factor
+    """
+    # 1. take step, obtain reward and new state
+    # (It should not depend no the ε-greedy value)
+    r, s_next = step_agent(s, a, gridworld, movements)
+    
+    # 2. Choose 'a_new' based on Q and 's_new'
+    a_next = policy_fn(s_next, Q, epsilon)
+
+    # 3. Take an update of the action-value
+    Q = Q.copy()
+    Q = update_fn(s, a, r, s_next, a_next, Q, epsilon, alpha, gamma)
+
+    return (s_next, a_next, r), Q
+
+
 def run_agent(
         ix_init,
         gridworld, n_actions, n_steps,
         epsilon, alpha, gamma, movements,
-        step_fn,
+        update_fn, policy_fn=None,
         seed=314, Q=None
 ):
+    if policy_fn is None:
+        policy_fn = choose_action
+
     if Q is None:
         Q = np.zeros((gridworld.n_states, n_actions))
 
@@ -190,19 +165,14 @@ def run_agent(
 
     for n in range(n_steps):
         set_seed(seed + n)
-        # 1. take step, obtain reward and new state
-        r, ix_new = step_agent(ix, action, gridworld, movements)
-        
-        # 2. Choose 'a_new' based on Q and 's_new'
-        action_new = choose_action(ix_new, Q, epsilon)
-    
-        # 3. Take a step fn
-        # Q = step_fn(s, a, r, s_next, a_next, Q, epsilon, alpha, gamma)
-        q_new = step_fn(ix, action, r, ix_new, action_new, Q, epsilon, alpha, gamma)
-        Q[ix, action] = q_new
-    
-        ix, action = ix_new, action_new
 
+        (ix, action, r), Q = step_and_update_agent(
+            Q, ix, action, epsilon, alpha, gamma, gridworld, movements, update_fn, policy_fn
+        )
+
+        ix_hist.append(ix)
+        action_hist.append(action)
+        reward_hist.append(r)
     
     ix_hist = np.array(ix_hist)
     action_hist = np.array(action_hist)
@@ -218,53 +188,111 @@ def run_agent(
 
 @njit
 def run_agent_return(
-    ix_init,
-    gridworld, n_actions, n_episodes,
-    epsilon, alpha, gamma, movements,
-    step_and_update_fn,
-    seed=314,
+        ix_init,
+        gridworld, n_actions, n_episodes,
+        epsilon, alpha, gamma, movements,
+        update_fn, policy_fn=None,
+        seed=314, Q=None
 ):
-    """
-    Run the agent until it reaches the goal n_episodes times
-    and return the total reward for each episode and the final Q-value function
-    """
-    Q = np.zeros((gridworld.n_states, n_actions))
+    if policy_fn is None:
+        policy_fn = choose_action
+
+    if Q is None:
+        Q = np.zeros((gridworld.n_states, n_actions))
+
     ix = ix_init
-    action = choose_action(ix, Q, epsilon)
+    action = choose_action(ix, Q, epsilon)   
 
     return_hist = np.zeros(n_episodes)
-
     t, n = 0, 0
     episode_return = 0.0
     while True:
         set_seed(seed + t)
-        (reward, ix, action), Q = step_and_update_fn(ix, action, Q, epsilon, alpha, gamma, gridworld, movements)
+        (ix, action, r), Q = step_and_update_agent(
+            Q, ix, action, epsilon, alpha, gamma, gridworld, movements, update_fn, policy_fn
+        )
 
-        episode_return += reward
-        # if gridworld.is_goal(ix) # TODO: refactor to this
-        if reward == gridworld.reward_goal:
+        episode_return += r
+        if r == gridworld.reward_goal:
             return_hist[n] = episode_return
             n = n + 1
             episode_return = 0.0
 
             if n == n_episodes:
                 break
-
+        
         t = t + 1
     return return_hist, Q
 
 
-
 @njit(parallel=True)
 def simulations_run_agent_return(
-    ix_init, gridworld, n_actions, n_episodes, epsilon, alpha, gamma, movements, step_and_update_fn,
-    n_simulations=100, seed=314,
+        ix_init,
+        gridworld, n_actions, n_episodes,
+        epsilon, alpha, gamma, movements,
+        update_fn, policy_fn=None,
+        seed=314, n_simulations=100, Q=None
 ):
     return_hist = np.zeros((n_simulations, n_episodes))
     for s in prange(n_simulations):
         return_hist[s], _ = run_agent_return(
             ix_init, gridworld, n_actions, n_episodes,
-            epsilon, alpha, gamma, movements, step_and_update_fn,
-            seed=seed + s
+            epsilon, alpha, gamma, movements, update_fn, policy_fn,
+            Q=Q, seed=seed + s
         )
     return return_hist
+
+
+# @njit
+# def run_agent_return(
+#     ix_init,
+#     gridworld, n_actions, n_episodes,
+#     epsilon, alpha, gamma, movements,
+#     step_and_update_fn,
+#     seed=314,
+# ):
+#     """
+#     Run the agent until it reaches the goal n_episodes times
+#     and return the total reward for each episode and the final Q-value function
+#     """
+#     Q = np.zeros((gridworld.n_states, n_actions))
+#     ix = ix_init
+#     action = choose_action(ix, Q, epsilon)
+
+#     return_hist = np.zeros(n_episodes)
+
+#     t, n = 0, 0
+#     episode_return = 0.0
+#     while True:
+#         set_seed(seed + t)
+#         (reward, ix, action), Q = step_and_update_fn(ix, action, Q, epsilon, alpha, gamma, gridworld, movements)
+
+#         episode_return += reward
+#         # if gridworld.is_goal(ix) # TODO: refactor to this
+#         if reward == gridworld.reward_goal:
+#             return_hist[n] = episode_return
+#             n = n + 1
+#             episode_return = 0.0
+
+#             if n == n_episodes:
+#                 break
+
+#         t = t + 1
+
+#     return return_hist, Q
+
+
+
+# @njit(parallel=True)
+# def simulations_run_agent_return(
+#     ix_init, gridworld, n_actions, n_episodes, epsilon, alpha, gamma, movements, step_and_update_fn,
+#     n_simulations=100, seed=314,
+# ):
+#     return_hist = np.zeros((n_simulations, n_episodes))
+#     for s in prange(n_simulations):
+#         return_hist[s], _ = run_agent_return(
+#             ix_init, gridworld, n_actions, n_episodes,
+#             epsilon, alpha, gamma, movements, step_and_update_fn,
+#             seed=seed + s
+#         )
+#     return return_hist
